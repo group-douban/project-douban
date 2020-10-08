6b311e1ca638803a4c90977fa6a128c1d325bb0f
mazhaoquan <quan22820@126.com>
2020-10-08 15:17:30 +0800

    final

--- douban/src/App.vue
+++ douban/src/App.vue
@@ -1,12 +1,7 @@
 <template>
-    <div>
-        <ul>
-            <router-link tag="li" to="/home">首页</router-link>
-            <router-link tag="li" to="/cinemaHit">影院热映</router-link>
-            <router-link tag="li" to="/movieList">榜单</router-link>
-        </ul>
+    
         <router-view></router-view>
-    </div>
+    
 </template>
 
 <script>
--- douban/src/router/index.js
+++ douban/src/router/index.js
@@ -3,27 +3,66 @@ import VueRouter from "vue-router";
 
 Vue.use (VueRouter);
 //导入组件
-import home from "../components/Home.vue";
-import cinemaHit from '../components/CinemaHit.vue';
-import movieList from '../components/MovieList.vue';
+import Home from "@v/index/Home.vue";
+import index from "@v/index/Index.vue";
 
 const routes = [
   {
     path:"/",
-    component:home,
+    name:'index',
+    component:index,
+    redirect:'/home',
+    children:[
+      {
+        path: "home",
+        name:'home',
+        component: Home,
+      },
+      {
+        path:"movieList",
+        name:'movieList',
+        component:()=>import(/* webpackChunkName: "movieList" */ '@v/index/MovieList.vue'),
+      },
+      {
+        path:"profile",
+        name:'profile',
+        component:()=>import(/* webpackChunkName: "profile" */ '@v/index/Profile.vue'),
+      },
+      {
+        path:'/hot',
+        name:'hot',
+        component:()=>import(/*webpackChunkName: "hot"*/ '@v/hot/Hot.vue'),
+        children:[
+          {
+            path:'cinemaHot',
+            name:'cinemaHot',
+            component:()=>import(/* webpackChunkName: "cinemaHot" */ '@v/hot/CinemaHot.vue'),
+          },{
+            path:'dbHot',
+            name:'dbHot',
+            component:()=>import(/* webpackChunkName: "dbHot" */ '@v/hot/DbHot.vue'),
+          },{
+            path:'recentHotSeries',
+            name:'recentHotSeries',
+            component:()=>import(/* webpackChunkName: "recentHotSeries" */ '@v/hot/RecentHotSeries.vue'),
+          },{
+            path:'recentHotVariety',
+            name:'recentHotVariety',
+            component:()=>import(/* webpackChunkName: "recentHotVariety" */ '@v/hot/RecentHotVariety.vue'),
+          },{
+            path:'bestSellingBook',
+            name:'bestSellingBook',
+            component:()=>import(/* webpackChunkName: "bestSellingBook" */ '@v/hot/BestSellingBook.vue'),
+          },{
+            path:'hotMusic',
+            name:'hotMusic',
+            component:()=>import(/* webpackChunkName: "hotMusic" */ '@v/hot/HotMusic.vue'),
+          },
+        ]
+      },
+    ]
   },
-  {
-    path: "/home",
-    component: home,
-  },
-  {
-    path:"/cinemaHit",
-    component:cinemaHit
-  },
-  {
-    path:"/movieList",
-    component:movieList
-  }
+  
 ];
 const router =new VueRouter({
   mode:'history',
